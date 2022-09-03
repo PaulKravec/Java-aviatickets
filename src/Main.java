8764e8e7a94a3139dddde7806cdcab7d5c4bd628
Paul_RazDva <paul1312white@gmail.com>
2022-09-03 12:01:11 +0300

    gitignore1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 200; // Стоимость билета
+        int miles = 20; // Стоимость одной мили
+
+        System.out.println(ticket/miles);
+
+
+    }
+}
